4ca59498269637eecba0c1ceb0cc7b473a9354a2
bjorn <tie.372@gmail.com>
2015-08-29 17:52:46 -0700

    Improve test output;

--- tests/runner.lua
+++ tests/runner.lua
@@ -7,18 +7,28 @@ end
 
 if arg[1] then
   arg[1] = arg[1]:gsub('^(tests/).+', ''):gsub('%.lua$', '')
-  return dofile('tests/' .. arg[1] .. '.lua')
-end
+  dofile('tests/' .. arg[1] .. '.lua')
+else
+  local files = {
+    'observer'
+  }
 
-local files = {
-  'observer'
-}
-
-for i, file in ipairs(files) do
-  dofile('tests/' .. file .. '.lua')
-  if next(files, i) then
-    print()
+  for i, file in ipairs(files) do
+    dofile('tests/' .. file .. '.lua')
+    if next(files, i) then
+      print()
+    end
   end
 end
+
+local red = string.char(27) .. '[31m'
+local green = string.char(27) .. '[32m'
+local normal = string.char(27) .. '[0m'
+
+if lust.errors > 0 then
+  io.write(red .. lust.errors .. normal .. ' failed, ')
+end
+
+print(green .. lust.passes .. normal .. ' passed')
 
 if lust.errors > 0 then os.exit(1) end

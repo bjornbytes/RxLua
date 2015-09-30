188860ece5b34e6c940376a40a212e56a1472834
bjorn <tie.372@gmail.com>
2015-09-29 21:56:08 -0700

    Test Observable.skipWhile;

---
+++ tests/skipWhile.lua
@@ -0,0 +1,24 @@
+describe('skipWhile', function()
+  it('produces an error if its parent errors', function()
+    local observable = Rx.Observable.fromValue(''):map(function(x) return x() end)
+    expect(observable.subscribe).to.fail()
+    expect(observable:skipWhile(function() end).subscribe).to.fail()
+  end)
+
+  it('uses the identity function if no predicate is specified', function()
+    local observable = Rx.Observable.fromTable({true, true, false}):skipWhile()
+    expect(observable).to.produce(false)
+  end)
+
+  it('produces values once the predicate returns false', function()
+    local function isEven(x) return x % 2 == 0 end
+    local observable = Rx.Observable.fromTable({2, 3, 4}, ipairs):skipWhile(isEven)
+    expect(observable).to.produce(3, 4)
+  end)
+
+  it('produces no values if the predicate never returns false', function()
+    local function isEven(x) return x % 2 == 0 end
+    local observable = Rx.Observable.fromTable({2, 4, 6}):skipWhile(isEven)
+    expect(observable).to.produce({})
+  end)
+end)
